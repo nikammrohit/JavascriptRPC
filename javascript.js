9a4e5a95047ef64777bff9fb5fdcc89d8f1c3533
nikammrohit <82987914+nikammrohit@users.noreply.github.com>
2024-09-01 17:13:57 -0500

    Made scoreboard a global variable so it does not reset to 0 each time it loops playRound. Also sepperated the code into seperate functions.

--- javascript.js
+++ javascript.js
@@ -1,10 +1,7 @@
 function getComputerChoice() {
 
-    userRounds = 'y';
-
-    while (userRounds === 'y') {
         
-        compNum = Math.floor(Math.random() * 2);
+    compNum = Math.floor(Math.random() * 3);
  //Assign # to computer choice string
     switch (compNum) {
         case 0:
@@ -19,31 +16,67 @@ function getComputerChoice() {
         default:
             break;
     }
+    
+    
+}
 
-    userChoice = prompt("Please Type Your Choice: \nRock\nPaper\nScissors\n");
+function getHumanChoice() {
+    //Get user choice and convert all characters to lowercase and then first character to upper
+    userChoice = prompt("Please Type Your Choice: \nRock\nPaper\nScissors\n").toLowerCase();
+    userChoice = userChoice.charAt(0).toUpperCase() + userChoice.slice(1);
+}
+
+let userWin = 0;
+let compWin = 0;
+
+function playRound(getHumanChoice, getComputerChoice) {
 //If user wins
     if (userChoice == "Rock" && compChoice == "Scissors" || 
         userChoice == "Paper" && compChoice == "Rock" || 
         userChoice == "Scissors" && compChoice == "Paper") {
         console.log("Congratulations! You Won\nThe computer chose:", compChoice, "and you chose:", userChoice);
+        userWin += 1;
+        console.log("Your Score:",userWin,"\nComputer Score:",compWin);
     }
+
 //If user loses
     else if (compChoice == "Rock" && userChoice == "Paper" || 
         compChoice == "Paper" && userChoice == "Scissors" || 
         compChoice == "Scissors" && userChoice == "Rock") {
         console.log("Sorry, you lost!\nThe computer chose:", compChoice, "\nYou chose:", userChoice);
+        compWin += 1;
+        console.log("Your Score:",userWin,"\nComputer Score:",compWin);
     }
+
 //If there is a tie
     else{
         console.log("Its a tie! You and the computer both chose", compChoice);
+        userWin += 1;
+        compWin += 1;
+        console.log("Your Score:",userWin,"\nComputer Score:",compWin);
     }
-//How many times the game will run
-    userRounds = prompt("Would you like to play again? (y/n):");
 
-        
-    }
-    
-    console.log("Thank you for playing!")
 }
 
-getComputerChoice();
+
+
+
+
+
+userRounds = 'y';
+
+while (userRounds === 'y') {
+
+    getComputerChoice();
+    getHumanChoice();
+    playRound();
+    
+
+    //How many times the game will run
+    userRounds = prompt("Would you like to play again? (y/n):");
+
+}
+
+console.log("Thank you for playing!");
+
+    
